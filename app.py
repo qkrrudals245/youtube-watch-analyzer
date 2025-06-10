2f2e3012d9164b63d9acc6e05cc3402886d82a3a
qkrrudals245 <ppemin77@naver.com>
2025-06-11 04:23:57 +0900

    app.py

--- app.py
+++ app.py
@@ -300,9 +300,6 @@ def index():
         plot_weekday=plot_weekday
     )
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
 
 
